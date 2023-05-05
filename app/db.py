c07c09b2081f8eed143678256809f6f37a4a324b
Gordon Mo <gmo30@stuy.edu>
2023-05-05 12:59:53 -0400

    user db funcs

--- app/db.py
+++ app/db.py
@@ -12,12 +12,14 @@ db.executescript("""
 CREATE TABLE if not exists userbase(username text, password text, wins int, losses int, recents text);
 """)
 
-def user_exists(username): #returns true if user EXISTS
+def user_exists(username): 
     c = db.cursor()
-    name = c.execute("Select user from userbase where user = ?", (username,))
+    name = c.execute("Select username from userbase where username = ?", (username,))
+    exists = name.fetchone() is not None #returns true if user EXISTS
     c.close()
-    name.fetchone() is not None 
-    
+    return exists
+
+print(user_exists("joe"))
 def add_user(username, password):
     c = db.cursor()
     if not user_exists(username):
@@ -26,4 +28,9 @@ def add_user(username, password):
 
 def check_pass(username, password):
     c = db.cursor()
+    c.execute('select password from userbase where (username = ? )', (str(username), ))
+    input_pass = c.fetchone()[1]
+    c.close()
+    return password == input_pass
+
     
