b4aef93f18526485ddffa97cc28f2b6bec45fc97
Gordon Mo <gmo30@stuy.edu>
2023-05-07 14:14:14 -0400

    fixed db?

--- app/db.py
+++ app/db.py
@@ -15,22 +15,23 @@ c.close()
 
 def user_exists(username): 
     c = db.cursor()
-    name = c.execute("Select username from userbase where username = ?", (username,))
+    name = c.execute("Select username from userbase where username = ?", (str(username),))
     exists = name.fetchone() is not None #returns true if user EXISTS
     c.close()
     return exists
 
-print(user_exists("joe"))
+#print(user_exists("joe"))
+
 def add_user(username, password):
     c = db.cursor()
-    if not user_exists(username):
-        c.execute("Insert into userbase values(?,?)", (username, password,))
+    c.execute("Insert into userbase values(?,?,?,?,?)", (str(username), str(password),0,0,""))
+    db.commit()
     c.close()
 
 def check_pass(username, password):
     c = db.cursor()
     c.execute('select password from userbase where (username = ? )', (str(username), ))
-    input_pass = c.fetchone()[1]
+    input_pass = c.fetchone()[0]
     c.close()
     return password == input_pass
 
