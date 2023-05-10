84b7c0f9414c1c768d29fbcbf3592e5be672adf9
Kevin Wang <kwang30@stuy.edu>
2023-05-10 17:33:28 -0400

    import navbar

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ symbol_list = ss.get_symbol_list(market='US',symbols_only=True)
 @app.route("/")
 @app.route("/home")
 def index():
+    return render_template('home.html')
     if 'username' in session:
         return render_template('home.html')
     return redirect(url_for('login'))
